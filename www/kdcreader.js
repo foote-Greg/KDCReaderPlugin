a95f507c881230a12412d4b6ea67b408af0d833b
foote-Greg <gfoote@meyerre.com>
2015-06-08 10:50:49 -0500

    Added power enable/disable to the js for the plugin

--- www/kdcreader.js
+++ www/kdcreader.js
@@ -10,12 +10,21 @@ var kdcreader =  {
             [{'nada':'nothing'}]
         );
     },
-    disableConnection: function (successCallback, errorCallback) {
+    disableNFCPower: function (successCallback, errorCallback) {
         cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
             'KDCPlugin', // mapped to our native Java class called "KDCReaderPlugin"
             'disableKDC',
+            [{'nada':'nothing'}]
+        );
+    },
+     enableNFCPower: function (successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'KDCPlugin', // mapped to our native Java class called "KDCReaderPlugin"
+            'enableKDC',
             [{'nada':'nothing'}]
         );
     }
